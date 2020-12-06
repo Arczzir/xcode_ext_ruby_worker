85c2696c590bc9a81bed6382cb1ecd2f39c88c5a
Arczzir <Arczzir@icloud.com>
2020-12-06 22:23:12 +0900

    path fix

--- scripts/cmt.rb
+++ scripts/cmt.rb
@@ -1,7 +1,6 @@
-
-path = "/Users/z/Downloads/xxx"
+path = File.expand_path "~/Downloads/xxx"
 x = $*[0]
 File.open(path, 'w') {|f|
     f << x
 }
-print `/Users/z/Downloads/commentExt #{path}`
+print `~/Downloads/commentExt "#{path}"`
